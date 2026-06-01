85872e2a2caf9069a17c0ee886a3ade35c838d45
Niklas Engström <memelord@1401.com>
2026-06-01 14:23:42 +0200

    feat: troop counts everywhere! usefeul!

--- yacfks/app/domains/army.py
+++ yacfks/app/domains/army.py
@@ -7,6 +7,11 @@ class ArmyLine:
     troop_type: TroopType
     troop_stacks: list[TroopStack]
 
+    # calc troop count as a property of the line, so we don't have to sum it up in lots of plcaes
+    @property
+    def troop_count(self) -> int:
+        return sum(stack.count for stack in self.troop_stacks)
+
     # after an army line is init:d, ensure its troop stacks are all of the same type as itself.
     def __post_init__(self):
         for stack in self.troop_stacks:
@@ -18,6 +23,28 @@ class Army:
     infantry_line: ArmyLine
     cavalry_line: ArmyLine
     archer_line: ArmyLine
+
+    # calc total troop count as a property of the army, so we don't have to sum it up in lots of plcaes
+    @property
+    def total_troop_count(self) -> int:
+        return(
+            self.infantry_line.troop_count
+            + self.cavalry_line.troop_count
+            + self.archer_line.troop_count
+        )
+
+    # calc troop count per troop type as properties
+    @property
+    def infantry_count(self) -> int:
+        return self.infantry_line.troop_count
+
+    @property
+    def cavalry_count(self) -> int:
+        return self.cavalry_line.troop_count
+
+    @property
+    def archer_count(self) -> int:
+        return self.archer_line.troop_count
 
     # after an army is init:d, ensure its lines are of the correct troop types.
     def __post_init__(self):
