16dfdfd8a42f750c735ff92c69e7b805370c919f
Niklas Engström <memelord@1401.com>
2026-06-01 14:23:01 +0200

    tests: more tests, fix some existing tests

--- yacfks/tests/test_army.py
+++ yacfks/tests/test_army.py
@@ -20,6 +20,7 @@ def test_army_line_rejects_mixed_troop_types():
             health=243
     ), count=5000)
 
+    # Infatry line cant contain cav, so we expect an error
     with pytest.raises(ValueError):
         ArmyLine(
             troop_type=TroopType.INF,
@@ -29,9 +30,10 @@ def test_army_line_rejects_mixed_troop_types():
             ]
         )
 
+    # arhcer line cant contain cav or inf, so we expect an error
     with pytest.raises(ValueError):
         ArmyLine(
-            troop_type=TroopType.INF,
+            troop_type=TroopType.ARCH,
             troop_stacks=[
                 inf_stack,
                 cav_stack
@@ -45,6 +47,8 @@ def test_army_rejects_mixed_line_types():
         troop_stacks=[]
     )
 
+    # basically same as aboe, but for Army, basic sanity test.
+    # An army cant have any army lines where troop types are mixed, e.g the inf line in an Army cant contain cavalry
     with pytest.raises(ValueError):
         Army(
             infantry_line=cav_line,
@@ -54,3 +58,110 @@ def test_army_rejects_mixed_line_types():
                 troop_stacks=[]
             )
         )
+
+# more explicit army test for mixed lines - inf line contains wrong troop type.
+def test_army_rejects_wrong_infantry_line():
+    cav_line = ArmyLine(
+        troop_type=TroopType.CAV,
+        troop_stacks=[]
+    )
+
+    with pytest.raises(ValueError):
+        Army(
+            infantry_line=cav_line,
+            cavalry_line=ArmyLine(
+                troop_type=TroopType.CAV,
+                troop_stacks=[]
+            ),
+            archer_line=ArmyLine(
+                troop_type=TroopType.ARCH,
+                troop_stacks=[]
+            )
+        )
+
+# more explicit army test for mixed lines - cav line contains wrong troop type.
+def test_army_rejects_wrong_cavalry_line():
+    inf_line = ArmyLine(
+        troop_type=TroopType.INF,
+        troop_stacks=[]
+    )
+
+    with pytest.raises(ValueError):
+        Army(
+            infantry_line=ArmyLine(
+                troop_type=TroopType.INF,
+                troop_stacks=[]
+            ),
+            cavalry_line=inf_line,
+            archer_line=ArmyLine(
+                troop_type=TroopType.ARCH,
+                troop_stacks=[]
+            )
+        )
+
+# more explicit army test for mixed lines - archer line contains wrong troop type.
+def test_army_rejects_wrong_archer_line():
+    inf_line = ArmyLine(
+        troop_type=TroopType.INF,
+        troop_stacks=[]
+    )
+
+    with pytest.raises(ValueError):
+        Army(
+            infantry_line=ArmyLine(
+                troop_type=TroopType.INF,
+                troop_stacks=[]
+            ),
+            cavalry_line=ArmyLine(
+                troop_type=TroopType.CAV,
+                troop_stacks=[]
+            ),
+            archer_line=inf_line
+        )
+
+def test_army_line_accepts_correct_line_types():
+    # test that an army with correct line types is accepted, basic sanity test.
+    inf_stack1 = TroopStack(
+        definition=make_troop_definition(
+            troop_type=TroopType.INF,
+            attack=243,
+            health=730
+    ), count=6000)
+
+    inf_stack2 = TroopStack(
+        definition=make_troop_definition(
+            troop_type=TroopType.INF,
+            tier_major=5,
+            tier_minor=0,
+            attack=206,
+            health=619
+    ), count=500)
+
+    line = ArmyLine(
+        troop_type=TroopType.INF,
+        troop_stacks=[
+            inf_stack1,
+            inf_stack2
+        ]
+    )
+
+    # the ArmyLine above should accept the 2 infantry troop stacks, so we check that the line has 2 stacks.
+    assert len(line.troop_stacks) == 2
+
+def test_army_accepts_correct_line_types():
+    army = Army(
+        infantry_line=ArmyLine(
+            troop_type=TroopType.INF,
+            troop_stacks=[],
+        ),
+        cavalry_line=ArmyLine(
+            troop_type=TroopType.CAV,
+            troop_stacks=[],
+        ),
+        archer_line=ArmyLine(
+            troop_type=TroopType.ARCH,
+            troop_stacks=[],
+        ),
+    )
+
+    assert army.infantry_line.troop_type == TroopType.INF
